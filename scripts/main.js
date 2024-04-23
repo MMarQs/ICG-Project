ba2fb8a5bf6afa91169e968adc7ffaacd50f7ad4
MMarQs <108202305+MMarQs@users.noreply.github.com>
2024-04-23 13:16:17 +0100

    Spaceship modelling

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,4 @@
 window.onload = () => {
-  // 1. 3D scene creation --------------------
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
@@ -7,26 +6,15 @@ window.onload = () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
 
   document.body.appendChild(renderer.domElement);
-  // -----------------------------------------
-  
-  // 2. 3D scene initialisation --------------
-  const geometry = new THREE.BoxGeometry();
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
-  
-  camera.position.z = 5;
-  // -----------------------------------------
-  
-  // 3. update loop function definition and initial call --------
+
+  const gameInstance = new Game(scene, camera);
+
   function animate() {
     requestAnimationFrame(animate);
-    
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    
+    // directly call the game instance method to
+    // be agnostic of all details
+    gameInstance.update();
     renderer.render(scene, camera);
   }
   animate();
-  // ------------------------------------------------------------
 }
